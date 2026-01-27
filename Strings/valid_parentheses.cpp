07e9f93d2515a5c99e7efe83ec36b3340020139b
Shreyansh Singh <singh.shreyansh1756@gmail.com>
2026-01-28 00:16:38 +0530

    20

---
+++ Strings/valid_parentheses.cpp
@@ -0,0 +1,24 @@
+// LeetCode 20 : Valid Parentheses
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto c:s){
+            if(c=='('){
+                st.push(')');
+            }
+            else if(c=='{'){
+                st.push('}');
+            }
+            else if(c=='['){
+                st.push(']');
+            }
+            else if(st.empty() || st.top()!=c){
+                return false;
+            }
+            else st.pop();
+        }
+        return st.empty();
+    }
+};
