0d03708fc932daa9a45a9d8caefe707c395f14a6
Shreyansh Singh <singh.shreyansh1756@gmail.com>
2026-01-29 21:51:46 +0530

    231

---
+++ Miscellaneous/power_of_two.cpp
@@ -0,0 +1,14 @@
+// LeetCode 231 : Power Of Two
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n%2==0){
+            n/=2;
+        }
+        return n==1;
+    }
+};
